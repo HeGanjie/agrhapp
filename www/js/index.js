713c674034d59e44b0ad6d48ba14eddda184f47b
heganjie <heganjie@gmail.com>
2016-01-15 15:00:00 +0800

    启动页面迁移至网页端

--- www/js/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('deviceready', function () {
-    var ref = cordova.InAppBrowser.open('http://www.agrhp2p.com/mobile/cordova-wrapper.html', '_self', 'location=no');
-    ref.addEventListener('exit', function () {
-        navigator.app.exitApp();
-    });
-}, false);
